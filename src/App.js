53bae0940aafa63fc6d03023104a961f02b293bf
Boneo, Matt Franky D. <127377325+MattBoneo-30@users.noreply.github.com>
2024-07-22 01:21:45 +0900

    about to go props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  const showEmployees = true; 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {showEmployees ? (
+         <>
+              <Employee />
+              <Employee />
+              <Employee />
+              <Employee />
+              <Employee />
+         </>
+
+        ) : (
+      <p>You cannot see the employees</p>
+    
+      )}
     </div>
   );
 }
