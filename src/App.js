78e329804190363a7de3a3398f5be285f5fcb70d
Boneo, Matt Franky D. <127377325+MattBoneo-30@users.noreply.github.com>
2024-07-22 18:34:03 +0900

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const[role, setRole] = useState('dev');
   const showEmployees = true; 
   return (
     <div className="App">
         {showEmployees ? (
          <>
-              <Employee />
-              <Employee />
-              <Employee />
+              <input 
+                  type="text" 
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setRole(e.target.value);
+                  }}
+              />
+              <Employee name=" Matt Boneo" role="Intern"/>
+              <Employee name=" Luffy" role={role} />
+              <Employee name=" Zoro"/>  
               <Employee />
               <Employee />
          </>
