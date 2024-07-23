547a7682e5231694fdb17549485058ee0ba7a637
Boneo, Matt Franky D. <127377325+MattBoneo-30@users.noreply.github.com>
2024-07-23 17:08:23 +0900

    added modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const[role, setRole] = useState('dev');
+  const[employees, setEmployees] = useState(
+    [
+      {
+        id: 1,
+        name: "Matt Boneo", 
+        role: "Developer", 
+        img: "https://scontent.fmnl25-3.fna.fbcdn.net/v/t39.30808-6/357737218_1696117677480462_3329789873805965179_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=hHbi-V2v2kEQ7kNvgGjUg9-&_nc_ht=scontent.fmnl25-3.fna&oh=00_AYBc79GPob563-9kRgbsknbEie-QMOpmAyQVD3QYvsdhEQ&oe=66A406E0"
+      },
+      {
+
+        name: "Luffy", 
+        role: "Captain", 
+        img: "https://m.supergeek.cl/noticias/site/artic/20230806/imag/foto_0000000420230806004515/nuffyy.jpg"
+      },
+      {
+        name: "Zoro", 
+        role: "Swordsman", 
+        img: "https://wallpapers-clan.com/wp-content/uploads/2024/02/one-piece-roronoa-zoro-with-sword-turquoise-desktop-wallpaper-preview.jpg"
+      },
+      {
+        name: "Nami", 
+        role: "Navigator", 
+        img: "https://wallpapers-clan.com/wp-content/uploads/2024/04/one-piece-nami-on-beach-desktop-wallpaper-preview.jpg"
+      },
+      {
+        name: "Robin", 
+        role: "Archaeologist", 
+        img: "https://wallpapers-clan.com/wp-content/uploads/2024/04/one-piece-nico-robin-desktop-wallpaper-preview.jpg"
+      },
+      {
+        name: "Sanji", 
+        role: "Cook", 
+        img: "https://wallpapers-clan.com/wp-content/uploads/2024/04/sanji-smoking-one-piece-desktop-wallpaper-preview.jpg"
+      },
+
+    ]);
+
   const showEmployees = true; 
   return (
     <div className="App">
@@ -16,11 +54,19 @@ function App() {
                     setRole(e.target.value);
                   }}
               />
-              <Employee name=" Matt Boneo" role="Intern"/>
-              <Employee name=" Luffy" role={role} />
-              <Employee name=" Zoro"/>  
-              <Employee />
-              <Employee />
+              <div className= "flex flex-wrap justify-center">
+                  {employees.map((employee) => {       
+                    console.log(uuidv4());               
+                    return(
+                        <Employee 
+                          key={uuidv4()}
+                          name={employee.name} 
+                          role={employee.role} 
+                          img={employee.img}
+                      />
+                      );
+                  })}
+              </div>
          </>
 
         ) : (
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,9 +1,26 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee(props){
     return (
-        <>
-            <h3>Employee {props.name}</h3>
-             <p>{props.role ? props.role : "No role"}</p>
-        </>
+        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img 
+                className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
+                src={props.img}
+            />
+            <div class="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p class="text-slate-500 font-medium">
+                        {props.role}
+                    </p>
+                </div>
+
+                <EditEmployee />
+
+            </div>
+        </div>
     );
 
 }
